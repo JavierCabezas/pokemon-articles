ad85d33ccdc1f50f267936184dd7f80a938c6f5b
Javier Cabezas <jcleyton@gmail.com>
2022-09-03 11:17:34 -0400

    Update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
         <ul class="icons">
             <li><a href="https://github.com/JavierCabezas" class="icon fa-github"><span class="label">GitHub</span></a>
             </li>
-            <li><a href="https://www.linkedin.com/in/javier-cabezas-a1bb6a116/" class="icon fa-linkedin"><span
+            <li><a href="https://www.linkedin.com/in/javier-cabezas-dev/" class="icon fa-linkedin"><span
                     class="label">LinkedIn</span></a></li>
             <li><a href="mailto:jcleyton@gmail.com" class="icon fa-envelope"><span class="label">E-mail</span></a>
             </li>
